b0f0fb033c1877dbc59885acd54b5aa1d9e39669
SimUI <133499867+Simonceeno@users.noreply.github.com>
2024-05-15 18:35:14 +0200

    Wrote another method to make animation possible
    Need to find a better way.

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,1 +1,11 @@
-let card = document.querySelector(".flipcard")
+let card = document.querySelector(".card")
+let frontCard = document.querySelector(".front-card")
+let backCard = document.querySelector(".back-card")
+
+flipCard = () =>{
+card.style.transition = "transform 1500ms";
+card.style.transform = "rotateY(180deg)";
+backCard.style.display = "flex"
+}
+
+card.addEventListener("click", flipCard);
